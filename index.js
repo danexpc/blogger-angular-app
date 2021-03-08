abd57471ec08d096acf4c6a7109a9b24521d4920
Danylo <danylosmahliuk@gmail.com>
2021-03-08 14:58:23 +0200

    Connecting to MongoDB Database

--- index.js
+++ index.js
@@ -4,15 +4,29 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
+const config = require('./config/db.config')
 
 const app = express();
 
 const port = 3000;
+
+mongoose.connect(config.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Successful connection to the database');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Not successful connection to the database ' + err);
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
 app.get('/', (req, res) => {
-    res.send('Home page...');
+    res.send('Home page');
 });
