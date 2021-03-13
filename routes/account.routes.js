917cb720450efa77df447d2d3fe03bfa98cceaa1
Danylo <danylosmahliuk@gmail.com>
2021-03-13 20:25:43 +0200

    Creating a user model

--- routes/account.routes.js
+++ routes/account.routes.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-router.get('/reg', (req, res) => {
-    res.send('Registration page');
+router.post('/reg', (req, res) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        login: req.body.login,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err) {
+            res.json({
+                success: false,
+                msg: 'User has not been added.'
+            })
+        } else {
+            res.json({
+                success: true,
+                msg: 'User has been added.'
+            })
+        }
+    })
 });
 
 router.get('/auth', (req, res) => {
